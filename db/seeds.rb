2829d35c4acb937c8b05eb97ee56c7ba28a97f1a
github username <github email address>
2018-06-18 16:59:21 -0400

    horrible

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(title: "Welcome to my post", content: "It is what it is")
