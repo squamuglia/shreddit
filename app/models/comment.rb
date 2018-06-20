8aac95ee849a7bc14e99e27a75d2cbe8e0032556
github username <github email address>
2018-06-20 14:40:44 -0400

    comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :likes
+  validates :content, presence: true
+  validates :content, length: {maximum: 500}
+  
 end
