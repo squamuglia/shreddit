811db86a802f5737ee64ed21b9c1bd248b305f80
squamuglia <maxsmouha@gmail.com>
2018-06-21 16:02:24 -0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Shreddit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
 
 
