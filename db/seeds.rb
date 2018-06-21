932c6b1ab6b03d6d91a30fa27a5c7fa39e59ea4e
squamuglia <maxsmouha@gmail.com>
2018-06-21 11:26:32 -0400

    merged tbone and max branches, added creators, validations, and administrators

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,11 @@ chef.update(slug: to_slug(chef.username))
 
 
 
-forum = Forum.create(name:"Fun Forum", slug: to_slug("Fun Forum"))
-forum2 = Forum.create(name:"Sales", slug: to_slug("Sales"))
-forum3 = Forum.create(name:"Tbone's Boneyard", slug: to_slug("Tbone's Boneyard"))
-forum4 = Forum.create(name:"Friends", slug: to_slug("Friends"))
+forum = Forum.create(name:"Fun Forum", slug: to_slug("Fun Forum"), user_id: 1)
+forum2 = Forum.create(name:"Sales", slug: to_slug("Sales"), user_id: 2)
+forum3 = Forum.create(name:"Tbone's Boneyard", slug: to_slug("Tbone's Boneyard"), user_id: 2)
+forum4 = Forum.create(name:"Friends", slug: to_slug("Friends"), user_id: 2)
+forum5 = Forum.create(name:"Max's Forum, A Space for Max, to be Max", slug: to_slug("Friends"), user_id: 1)
 
 
 p1 = Post.create(title: "Welcome to my post", content: "It is what it is", user_id:max.id, forum_id: forum.id)
